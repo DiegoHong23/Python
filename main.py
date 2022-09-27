8c287db1a7c49332881485afe84138c65d20961f
DiegoHong23 <diegohong2309@gmail.com>
2022-09-27 21:02:29 +0800

    replit

---
+++ main.py
@@ -0,0 +1,15 @@
+animal = input('Please input "Hamster", "Gerbil" or "Snake": ')
+weight = int(input('Please input number of animal weight: '))
+if (animal == "Hamster") or  (animal == "Gerbil"): 
+    if (weight >= 60) and (weight <= 90):
+        print("accepted")
+    else:
+        print(animal, "weight is outside of the health range.")
+elif (animal == "Snake"):
+    if (weight >= 800) and (weight <= 900):
+        print("accepted")
+    else:
+        print(animal, "weight is outside of the health range.")
+
+
+
